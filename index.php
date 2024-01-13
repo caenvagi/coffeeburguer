c17f58a5d1786f81513e3a32ffec2149035de7c0
Carlos Valencia <caenvagi@hotmail.com>
2024-01-13 00:26:39 -0500

    Se modifica el card loguin para movil 	modified:   css/estilos.css 	modified:   css/styles.css 	modified:   index.php

--- index.php
+++ index.php
@@ -50,13 +50,10 @@ if ($_POST) {
 }
 
 echo "<link rel='stylesheet' type='text/css' href='css/estilos.css'>";
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -75,97 +72,91 @@ echo "<link rel='stylesheet' type='text/css' href='css/estilos.css'>";
 	<link href="css/styles.css" rel="stylesheet" />
 </head>
 
-<body id="fondo" class="fondo">
-<main>
-	<div id="layoutAuthentication">
-		<div id="layoutAuthentication_content">
-			
-				<div class="container mt-3">
-					<div class=" row justify-content-center mt-0 mb-0">
-						<div class="col-lg-5 mt-3 mb-3">
-							<div class="TB card shadow-lg border-0 rounded-lg mt-0 mb-0">
-								<div class="card-header mt-0">
-									<div >
-										<img  id="logo-loguin" src="assets/img/logo.png">
+<body >
+	<main>
+		<div id="layoutAuthentication">
+			<div class="row justify-content-center fondo" id="layoutAuthentication_content">
+				
+						<!-- <div id="fondo" class="row justify-content-center fondo"> -->
+							<div class="col-lg-5 col-10">
+								<div class="card shadow-lg border-0 rounded-lg mt-5 mb-0" id="card-loguin">
+									<div class="card-header">
+										<div >
+											<img  id="logo-loguin" src="assets/img/logo.png">
+										</div>
+										<h4 class="text-center font-weight-light my-4">Coffee Burger "Mirador"</h4>
 									</div>
-									<h4 class="text-center font-weight-light my-4">Coffee Burger "Mirador"</h4>
-								</div>
-								<div class="card-body">
-									<!-- Mensaje error password -->
-									<?php
-									if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'password') {
-									?>
-										<div class="alerta alert alert-danger alert-dismissible fade show" role="alert">
-											<strong>Error !</strong> Password Incorrecto
-											<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-										</div>
-									<?php
-									}
-									?>
-									<!-- fin Mensaje error password -->
-									<!-- Mensaje error usuario -->
-									<?php
-									if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'usuario') {
-									?>
-										<div class="alerta alert alert-warning alert-dismissible fade show" role="alert">
-											<strong>Error !</strong> Usuario Incorrecto
-											<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-										</div>
-									<?php
-									}
-									?>
-									<!-- fin Mensaje error usuario -->
-									<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-
-										<div class="input-group  mb-3">
-											<div class="input-group-prepend">
-												<span class="input-group-text" id="basic-addon1"><i class='fas fa-user-circle'></i>&nbsp;</span>
+									<div class="card-body">
+									<!-- Mensajes -->	
+										<!-- Mensaje error password -->
+										<?php
+										if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'password') {
+										?>
+											<div class="alerta alert alert-danger alert-dismissible fade show" role="alert">
+												<strong>Error !</strong> Password Incorrecto
+												<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 											</div>
-											<input type="text" class="form-control" id="usuario" name="usuario" placeholder="usuario" aria-label="usuario" aria-describedby="basic-addon1">
-										</div>
-
-										<!-- <div class="form-floating mb-3">
-											<input class="form-control" id="usuario" name="usuario" type="text" placeholder="usuario" />
-											<label for="Usuario">Usuario</label>
-										</div> -->
-
-										<div class="input-group  mb-3">
-											<div class="input-group-prepend">
-												<span class="input-group-text" id="basic-addon1"><i class='fas fa-key'></i>&nbsp;</span>
+										<?php
+										}
+										?>
+										<!-- fin Mensaje error password -->
+										<!-- Mensaje error usuario -->
+										<?php
+										if (isset($_GET['mensaje']) and $_GET['mensaje'] == 'usuario') {
+										?>
+											<div class="alerta alert alert-warning alert-dismissible fade show" role="alert">
+												<strong>Error !</strong> Usuario Incorrecto
+												<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 											</div>
-											<input type="password" id="clave" class="form-control" name="clave" placeholder="password" aria-label="password" aria-describedby="basic-addon1">
-										</div>
-
-										<!-- <div class="form-floating mb-3">
-											<input class="form-control" id="inputPassword" name="password" type="password" placeholder="Password" />
-											<label for="inputPassword">Password</label>
-										</div> -->
-										<!-- <div class="form-check mb-3">
-											<input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
-											<label class="form-check-label" for="inputRememberPassword">Remember Password</label>
-										</div> -->
-										<div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-											<!-- <a class="small" href="password.html">Forgot Password?</a> -->
-											<button type="submit" class="btn btn-primary">Iniciar Sesion</button>
-										</div>
-									</form>
+										<?php
+										}
+										?>
+										<!-- fin Mensaje error usuario -->
+									<!-- Mensajes -->
+										<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+											<div class="input-group  mb-3">
+												<div class="input-group-prepend">
+													<span class="input-group-text" id="basic-addon1"><i class='fas fa-user-circle'></i>&nbsp;</span>
+												</div>
+												<input type="text" class="form-control" id="usuario" name="usuario" placeholder="usuario" aria-label="usuario" aria-describedby="basic-addon1">
+											</div>
+											<!-- <div class="form-floating mb-3">
+												<input class="form-control" id="usuario" name="usuario" type="text" placeholder="usuario" />
+												<label for="Usuario">Usuario</label>
+											</div> -->
+											<div class="input-group  mb-3">
+												<div class="input-group-prepend">
+													<span class="input-group-text" id="basic-addon1"><i class='fas fa-key'></i>&nbsp;</span>
+												</div>
+												<input type="password" id="clave" class="form-control" name="clave" placeholder="password" aria-label="password" aria-describedby="basic-addon1">
+											</div>
+											<!-- <div class="form-floating mb-3">
+												<input class="form-control" id="inputPassword" name="password" type="password" placeholder="Password" />
+												<label for="inputPassword">Password</label>
+											</div> -->
+											<!-- <div class="form-check mb-3">
+												<input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
+												<label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+											</div> -->
+											<div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+												<!-- <a class="small" href="password.html">Forgot Password?</a> -->
+												<button type="submit" class="btn btn-primary">Iniciar Sesion</button>
+											</div>
+										</form>
+									</div>
+									<!-- <div class="card-footer text-center py-3">
+										<div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+									</div> -->
 								</div>
-								<!-- <div class="card-footer text-center py-3">
-									<div class="small"><a href="register.html">Need an account? Sign up!</a></div>
-								</div> -->
 							</div>
-						</div>
-					</div>
-				</div>
-			
+						<!-- </div> -->
+			</div>
+				<?php
+				require 'logs/nav-footer.php';
+				?>
 		</div>
-		<?php
-		require 'logs/nav-footer.php';
-		?>
-	</div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-	<script src="js/scripts.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+			<script src="js/scripts.js"></script>
 	</main>
 </body>
-
 </html>
